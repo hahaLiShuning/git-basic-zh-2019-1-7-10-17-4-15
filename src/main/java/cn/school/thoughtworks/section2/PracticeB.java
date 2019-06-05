7fc1cad1de237b6661608cd3237c966f2a79a76f
LIShuning <lishiuning1955142623@outlook.com>
2019-06-05 23:29:03 +0800

    Practice B of section 2 completed

--- src/main/java/cn/school/thoughtworks/section2/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeB.java
@@ -1,12 +1,30 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeB {
     Map<String, Integer> countSameElements(List<String> collection1) {
         //实现练习要求，并改写该行代码。
+        HashMap<String, Integer> result = new HashMap<>();
+        for (String item : collection1) {
+            if (item.contains("-")) {
+                String key = item.substring(0, 1);
+                Integer value = Integer.valueOf(item.substring(2));
+                PracticeB.setValueForMap(result, key, value);
+            } else {
+                PracticeB.setValueForMap(result, item, 1);
+            }
+        }
+        return result;
+    }
 
-        return null;
+    static void setValueForMap(HashMap<String, Integer> map, String key, Integer value) {
+        if (map.get(key) == null) {
+            map.put(key, value);
+        } else {
+            map.put(key, map.get(key) + value);
+        }
     }
 }
