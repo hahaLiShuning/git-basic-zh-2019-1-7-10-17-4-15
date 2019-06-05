f3f0c6dfd1d613fc9a71cb92d7436b5f57b7c42d
LIShuning <lishiuning1955142623@outlook.com>
2019-06-05 23:24:42 +0800

    Practice B of section 1 completed

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,14 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        List<String> result = new ArrayList<>();
+        collection2.forEach(list -> result.addAll(collection1.stream().filter(item -> list.contains(item)).collect(Collectors.toList())));
+        return result;
     }
 }
