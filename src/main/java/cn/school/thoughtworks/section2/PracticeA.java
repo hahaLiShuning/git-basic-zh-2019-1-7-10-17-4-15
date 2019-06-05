ce90a5122176c7372aa73865eeeace53556057cc
LIShuning <lishiuning1955142623@outlook.com>
2019-06-05 23:28:32 +0800

    format Code

--- src/main/java/cn/school/thoughtworks/section2/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeA.java
@@ -7,7 +7,6 @@ import java.util.stream.Collectors;
 public class PracticeA {
     Map<String,Integer> countSameElements(List<String> collection1) {
         //实现练习要求，并改写该行代码。
-
         return collection1.stream().collect(Collectors.toMap(s->s, s->1, Integer::sum));
     }
 }
